b03a493e7f685351991f64ff11367ce23b68813f
Jordan <hammerleejordan@gmail.com>
2021-10-01 04:05:25 -0700

    remote web

---
+++ Inventory.Remote/Controllers/SearchProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Inventory.Core;
+using Inventory.DataAccess;
+
+namespace Inventory.Remote.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchProductController : ControllerBase
+    {
+        private readonly ProductSearchEngine searchEngine;
+
+        public SearchProductController(ProductSearchEngine searchEngine)
+        {
+            this.searchEngine = searchEngine;
+        }
+
+        [HttpGet]
+        public IEnumerable<ProductModel> Get(string search)
+        {
+            return searchEngine.SearchResults(search).Take(50);
+        }
+
+
+    }
+}
---
+++ Inventory.Remote/Startup.cs
@@ -0,0 +1,100 @@
+using Inventory.Remote.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Inventory.Core;
+using Inventory.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+namespace Inventory.Remote
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            services
+                .AddTransient<ISqlLiteDataAccess, SqlLiteDataAccess>()
+                .AddTransient<ProductSearchEngine>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication", Version = "v1" });
+            });
+
+            services.AddHttpClient();
+
+            AddDbConfig(config,services);
+        }
+
+        private static void AddDbConfig(IConfiguration config, IServiceCollection serviceCollection)
+        {
+            string pathToDb = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            pathToDb = Path.Join(pathToDb, config["DbLocation"]);
+            var dbConnection = new DbConnection(pathToDb);
+
+            serviceCollection.AddSingleton(dbConnection);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication v1"));
+
+
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+
+
+        }
+    }
+}
